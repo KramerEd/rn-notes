3d2515b4a51e4dbfeb03e4a1f0313e314743d244
KramerEd <danat7531@gmail.com>
2022-09-23 15:23:35 +0300

    init

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{backgroundColor: "white", flex:1}}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
